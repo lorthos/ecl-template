af567ab7da7d24d9f0221e7f37fccf90ad2c82e5
Halit Olali <halitolali@gmail.com>
2017-02-28 21:39:10 +0100

    sync

--- src/ecltemplate/cpp/ui.h
+++ src/ecltemplate/cpp/ui.h
@@ -1,13 +1,15 @@
-#ifndef UI_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#ifndef UI_H
 #define UI_H
 
 #include <unistd.h>
 #include <sstream>
 #include <string>
 
-//#include <GL/glew.h>
-//#include <GLFW/glfw3.h>
 #include <SFML/Window.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 
 int ogl_w();
 
